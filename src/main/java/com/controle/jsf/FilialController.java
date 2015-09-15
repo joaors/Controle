fbd79deda7ed83ca901bed9321ec4d7576999cf1
João Rodrigo <joao.rodrigo@live.com>
2015-09-15 15:00:54 -0300

    Componente para botões do Form e botão novo.

--- src/main/java/com/controle/jsf/FilialController.java
+++ src/main/java/com/controle/jsf/FilialController.java
@@ -2,12 +2,10 @@ package com.controle.jsf;
 
 import com.controle.entity.Filial;
 import com.controle.facade.FilialFacade;
-import java.util.ArrayList;
 
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
-import javax.persistence.criteria.Predicate;
 
 @Named
 @ViewScoped
